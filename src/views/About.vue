558279a9142388b6a26ced00fc66204855c57e24
mgkyawmin <kyawmintun.kmt4142020@gmail.com>
2024-07-28 02:59:34 +0700

    initial setup

---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1>Hello welcome to About Page</h1>
+      <div class="container mt-4">
+          <div class="row">
+            <div class="col" cols="4">
+              <!-- <MovieCard :movie="movie" /> -->
+            </div>
+          </div>
+        </div>
+    </div>
+  </template>
+    
+    <script>
+    export default {
+    }
+    </script>
+  
+    
